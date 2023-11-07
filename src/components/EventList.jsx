5f63fb032221859d7ae6ce30e28e94ee881f30cd
Kevin <kevinolongo9@gmail.com>
2023-11-08 02:06:28 +0300

    Display event details

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -10,24 +10,7 @@ const EventList = () => {
       .catch((error) => console.error('Error fetching events:', error));
   }, []);
 
-  const handleBuyTicket = (event) => {
-    const availableTickets = event.tickets_available;
-
-    if (availableTickets > 0) {
-      alert('You have bought a ticket for ' + event.name);
-    
-      fetch(`http://localhost:3000/events/${event.id}`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({tickets_available: availableTickets - 1,}),
-      });
-    } else {
-      alert('Tickets are sold out for this event!');
-      return; 
-    }
-  };
+  
 
   return (
     <div>
@@ -35,10 +18,9 @@ const EventList = () => {
       {events.map((event) => (
         <div className='EventCard' key={event.id}>
           <h3>{event.name}</h3>
-          <p className='description'>{event.description}</p>
+          
           <img className='image' src={event.image_url} alt={event.name} />
-          <br />
-          <button  className='BtnBuy'onClick={() => handleBuyTicket(event)}>Buy Ticket</button>
+          
         </div>
       ))}
     </div>
